e8a8301e07dff7b6791565f9f0070e8abd5103de
palm <tauman.exe@gmail.com>
2019-01-03 14:17:04 +0100

    Modified Wave preview to show pictures, and have a tooltip for more detailed information

--- Assets/src/WaveManagement/WavePreview.cs
+++ Assets/src/WaveManagement/WavePreview.cs
@@ -7,7 +7,7 @@ namespace WaveManagement
 {
     public class WavePreview : MonoBehaviour
     {
-        public Text text;
+        public Image[] images;
 
         public void Start()
         {
@@ -20,18 +20,35 @@ namespace WaveManagement
 
         public void ShowWave(Army.Wave wave)
         {
-            text.gameObject.SetActive(true);
-            var format = "Next";
-            foreach (var item in wave.units)
+            int hpMult = 100;
+            if (DifficultySelector.Difficulty == Difficulty.easy)
+                hpMult = 70;
+            if (DifficultySelector.Difficulty == Difficulty.medium)
+                hpMult = 80;
+
+            Hide();
+            for (int i = 0; i < wave.units.Count; i++)
             {
-                format += "\n" + item.qty + "x " +  item.enemy.name;
+                var enemy = wave.units[i].enemy.GetComponent<Attack.Enemy>();
+                images[i].sprite = enemy.GetComponentInChildren<SpriteRenderer>(true).sprite;
+                images[i].gameObject.SetActive(true);
+                var text = images[i].GetComponentInChildren<Text>(true);
+                var tooltip = $"<b>{wave.units[i].qty}x {enemy.name}</b> ({enemy.hits * hpMult} hp)";
+                if (enemy.GetComponent<Attack.Enemy>().armor)
+                    tooltip += "\nArmor: \u00BD Physical Damage.";
+                var regen = enemy.GetComponent<Attack.Regeration>();
+                if (regen)
+                    tooltip += $"\nRegeneration: {regen.hpPerSecond} hp/seconds.";
+                text.text = tooltip;
             }
-            text.text = format;
         }
 
         public void Hide()
         {
-            text.gameObject.SetActive(false);
+            foreach (var image in images)
+            {
+                image.gameObject.SetActive(false);
+            }
         }
     }
 }
