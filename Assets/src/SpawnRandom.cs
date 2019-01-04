c3b3da00c20e4d9a018122c5c13797bda6f75e8b
palm <tauman.exe@gmail.com>
2019-01-04 16:31:04 +0100

    Animated a bunch of ponies to replace the Changeling and Diamond Dog

---
+++ Assets/src/SpawnRandom.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandom : MonoBehaviour {
+
+    public GameObject[] prefabs;
+    public int qty = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < qty; i++)
+        {
+            var o = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+            o.transform.position = transform.position;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
